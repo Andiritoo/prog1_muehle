9c53efc557b7dc5288780fe6284755530eced41e
Nathanael Liechti <technat@technat.ch>
2025-12-02 13:41:40 +0100

    feat: basic bot algo

---
+++ src/main/java/com/github/Andiritoo/BotPlayer/BotPlayer.java
@@ -0,0 +1,11 @@
+package com.github.Andiritoo.BotPlayer;
+
+public class BotPlayer {
+
+  // 3 sections: place, solve and survive
+
+  // Basic requirements: select a next valid field randomly
+
+  // Ultimate requirement: calculate the next step based on all valid Options and find valid options
+  // where the opponent doesn't immediately have the chance to make a point
+}
